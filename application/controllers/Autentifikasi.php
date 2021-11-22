6d651106c0b02ff616313a73e438635a61b7f53c
tanniadwikarina <karinaatannia01@gmail.com>
2021-11-22 11:28:18 +0700

    menambah public function blok dan gagal di controllers/autentifikasi

--- application/controllers/Autentifikasi.php
+++ application/controllers/Autentifikasi.php
@@ -56,4 +56,14 @@ class Autentifikasi extends CI_Controller
     }
 }
 
+public function blok()
+ {
+ $this->load->view('autentifikasi/blok');
+ }
+ public function gagal()
+ {
+ $this->load->view('autentifikasi/gagal');
+ }
+
+
 }
